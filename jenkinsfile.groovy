119e999421c433ef2eda49eaf9cb14ecfe169e6d
Maheshm123 <ms.maheshhwari@gmail.com>
2021-05-24 22:29:05 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,6 +5,7 @@ pipeline {
             steps {
             sh "git branch: 'main', credentialsId: 'Mahesh_Git', url: 'https://github.com/Maheshm123/MediaWiki_CFT_Ansible.git'"
               }
+        }
         stage('Submit Stack') {
             steps {
             sh "/usr/local/bin/aws cloudformation create-stack --stack-name Blue-Green-deploymentStack --template-body file://./blueGreen.yaml --region 'us-east-1'"
