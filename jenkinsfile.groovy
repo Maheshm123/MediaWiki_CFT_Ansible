3cd500fb5a109ca3940153a7a3cb63b4fbbf8adb
Maheshm123 <ms.maheshhwari@gmail.com>
2021-05-25 00:50:52 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,13 +1,7 @@
 pipeline {
     agent any
     stages {
-        #stage('Submit Stack') {
-         #   steps {
-          #  sh "/usr/local/bin/aws cloudformation create-stack --stack-name Blue-Green-deploymentStack --template-body file://./blueGreen.yaml --region 'us-east-1'"
-           #   }
-           #
-            # }
-        stage('Dynamically create the inventory file for ansible') {
+         stage('Dynamically create the inventory file for ansible') {
             steps {
             sh "python dynamicInventory.py Blue-Green-deploymentStack us-east-1"
               }
