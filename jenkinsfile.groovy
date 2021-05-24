bb39777fa06bc20ea0f61fc2a020254424f326bf
Maheshm123 <ms.maheshhwari@gmail.com>
2021-05-24 18:48:21 +0530

    Initial Commit

---
+++ jenkinsfile.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
+
+Install AWS CLI
+	SSH and Install AWS CLI
+	Use SSM Session Manager to install wihout keys
+Jenkins File
+Run Cft using Jenkins pipeline
+	Uses AWS CLI Commands
