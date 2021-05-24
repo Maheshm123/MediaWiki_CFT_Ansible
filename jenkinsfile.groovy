fbfd9980d9487a3046b64fa0601ee07fe6c19a06
Maheshm123 <ms.maheshhwari@gmail.com>
2021-05-24 22:16:59 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Submit Stack') {
             steps {
-            sh "/usr/local/bin/aws cloudformation create-stack --stack-name demo-stack --template-body file://./blueGreen.yaml --region 'us-east-1'"
+            sh "/usr/local/bin/aws cloudformation update-stack --stack-name demo-stack --template-body file://./blueGreen.yaml --region 'us-east-1'"
               }
              }
             }
